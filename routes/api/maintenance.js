ee4484ebe560761e17fb29708e3a923c44855142
Quentin Xiong <que.xiong@yahoo.com>
2020-01-24 11:51:30 -0800

    added maintenance delete route, get all maintenance jobs route

--- routes/api/maintenance.js
+++ routes/api/maintenance.js
@@ -8,10 +8,47 @@ const Maintenance = require("../../models/Maintenance");
 const Car = require("../../models/Car");
 const User = require("../../models/User");
 
+// @route       GET api/maintenance/:id
+// @description Get all maintenance items for a user
+// @access type Private
+// Retrieve all maintenance items related to specific user - will use this to populate the client views
+router.get("/:user_id", auth, async (req, res) => {
+  try {
+    let maintenance = await Maintenance.find({
+      user: req.params.user_id
+    });
+    if (maintenance.length == 0) {
+      return res
+        .status(400)
+        .json({ msg: "There are no maintenance jobs for this car" });
+    }
+    res.json(maintenance);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
 // @route       GET api/maintenance/:car_id
 // @description Get all maintenance items for a car
 // @access type Private
 // Retrieve all maintenance items related to specific car - will use this to populate the client views
+// router.get("/:car_id", auth, async (req, res) => {
+//   try {
+//     let maintenance = await Maintenance.find({
+//       car: req.params.car_id
+//     });
+//     if (maintenance.length == 0) {
+//       return res
+//         .status(400)
+//         .json({ msg: "There are no maintenance jobs for this car" });
+//     }
+//     res.json(maintenance);
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).send("Server error");
+//   }
+// });
 
 // @route       POST api/maintenance/:car_id
 // @description Add maintenance job to selected car
@@ -64,7 +101,7 @@ router.post(
 // @description Edit selected maintenance job on selected car
 // @access type Private
 router.put(
-  "/:car_id/:maint_id",
+  "/:car_id/:id",
   [
     auth,
     [
@@ -88,7 +125,7 @@ router.put(
     try {
       const { mileage, job, cost, notes } = req.body;
       let newMaintItemFields = {};
-      newMaintItemFields.id = req.params.maint_id;
+      newMaintItemFields.id = req.params.id;
       newMaintItemFields.user = req.user.id;
       newMaintItemFields.car = req.params.car_id;
       newMaintItemFields.mileage = mileage;
@@ -112,5 +149,22 @@ router.put(
     }
   }
 );
+
+// @route       DELETE api/maintenance/:id
+// @description Delete maintenance job by id
+// @access      Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let maintenance = await Maintenance.findById(req.params.id);
+    if (!maintenance) {
+      return res.status(404).json({ msg: "Maintenance job not found" });
+    }
+    await maintenance.remove();
+    res.json({ msg: "Maintenance job was removed" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
