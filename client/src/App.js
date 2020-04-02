d921b9641eadcfef589b9553d65f46b1ac586b05
Quentin Xiong <que.xiong@yahoo.com>
2020-04-01 21:58:33 -0700

    redux store created

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,26 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import "./App.css";
+// Redux - in order to use redux store, must use a PROVIDER from the react-redux package, which connects redux and react
+// Must wrap everything with <Provider></Provider> so all components created can access app level state
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <NavBar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
